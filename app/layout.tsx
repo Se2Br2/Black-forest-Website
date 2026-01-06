5a33cdcc405cf73fc66f461cc8b3e5c50108c4e7
Se2Br2 <deniska22072001@gmail.com>
2026-01-06 23:40:23 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,14 +45,10 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon-light.png", type: "image/png", media: "(prefers-color-scheme: light)" },
       { url: "/favicon-dark.png", type: "image/png", media: "(prefers-color-scheme: dark)" },
-      // запасной вариант, если браузер не поддерживает prefers-color-scheme для favicon
-      { url: "/favicon-light.png", type: "image/png" },
     ],
-    // иконка для iOS (на экран "Домой")
     apple: "/apple-touch-icon.png",
   },
 }
-
 
 export default function RootLayout({
   children,
